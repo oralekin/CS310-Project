7c1f711f3fdcacadda01fc41690744eebef28e69
Baharkucukozer <bahar.kucukozer@sabanciuniv.edu>
2025-11-25 22:55:09 +0300

    Update splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'home screen.dart';
+import 'login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
+  static const routeName = "/splash";
+
   const SplashScreen({super.key});
 
   @override
@@ -9,17 +11,12 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
   void initState() {
     super.initState();
 
-
     Future.delayed(const Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const UserHomeScreen()),
-      );
+      Navigator.pushReplacementNamed(context, LoginScreen.routeName);
     });
   }
 
@@ -27,56 +24,31 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            const Spacer(),
-
-            const Text(
+          children: const [
+            Spacer(),
+            Text(
               "UniConnect",
               style: TextStyle(
                 fontSize: 32,
                 fontWeight: FontWeight.bold,
-                color: Colors.black87,
               ),
             ),
-
-            const SizedBox(height: 12),
-
-            const Text(
+            SizedBox(height: 10),
+            Text(
               "Connecting University Clubs",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
+              style: TextStyle(color: Colors.grey),
             ),
-
-            const Spacer(),
-
-            // Large gray circle (placeholder)
-            Container(
-              width: 90,
-              height: 90,
-              decoration: const BoxDecoration(
-                color: Color(0xFFD3D3D3),
-                shape: BoxShape.circle,
-              ),
+            Spacer(),
+            CircleAvatar(
+              radius: 45,
+              backgroundColor: Colors.grey,
             ),
-
-            const SizedBox(height: 18),
-
-            const Text(
-              "Loading...",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-            ),
-
-            const Spacer(),
+            SizedBox(height: 20),
+            Text("Loading...", style: TextStyle(color: Colors.grey)),
+            Spacer(),
           ],
         ),
       ),
